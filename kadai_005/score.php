ba916cdaa37477e93a08123299308a12d7d5d03c
yuisaaan <167851292+yuisaaan@users.noreply.github.com>
2024-07-29 18:55:52 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+  <p>
+
+<?php
+ //変数に値を代入する//
+ $score1=80;
+ $score2=60;
+ $score3=55;
+ $score4=40;
+ $score5=100;
+ $score6=25;
+ $score7=80;
+ $score8=95;
+ $score9=30;
+ $score10=60;
+
+ //10人の合計点
+ $total_score='score1+score2+score3+score4+score5+score6+score7+score8+score9+score10';
+
+ //10人の平均点
+ $average='total_score';
+
+ //変数の中身を出力する
+ echo $average;
+ ?>
+ </p>
+</body>
+
+</html>
