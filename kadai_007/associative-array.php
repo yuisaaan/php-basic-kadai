5afbb2ad3746ae2dee40d7bc6e4a4ef53e8e0144
yuisaaan <167851292+yuisaaan@users.noreply.github.com>
2024-07-31 09:52:22 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>連想配列</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    //連想配列に値を代入する
+    $basic_data=['name'=>'onion','price'=>200,'weight'=>160];
+
+    //連想配列の値を出力する
+    print_r($basic_data);
